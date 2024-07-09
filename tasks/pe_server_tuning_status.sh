ec255dd8c59ff4414d59437a777790e4013b8a0b
Andrew_demo <andrew.lestrange@puppet.com>
2024-07-09 13:32:42 +1000

    adding tuning task

---
+++ tasks/pe_server_tuning_status.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Puppet Task Name: pe_server_tuning_status
+#
+# This is where you put the shell code for your task.
+#
+# You can write Puppet tasks in any language you want and it's easy to
+# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
+# https://puppet.com/docs/bolt/0.x/writing_tasks.html
+#
+# Puppet tasks make it easy for you to enable others to use your script. Tasks
+# describe what it does, explains parameters and which are required or optional,
+# as well as validates parameter type. For examples, if parameter "instances"
+# must be an integer and the optional "datacenter" parameter must be one of
+# portland, sydney, belfast or singapore then the .json file
+# would include:
+#   "parameters": {
+#     "instances": {
+#       "description": "Number of instances to create",
+#       "type": "Integer"
+#     },
+#     "datacenter": {
+#       "description": "Datacenter where instances will be created",
+#       "type": "Enum[portland, sydney, belfast, singapore]"
+#     }
+#   }
+# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
+#
+if [ -d $PT_output_dir ]
+then
+    if [ ! -d "$PT_output_dir/pe_quick_data" ]
+    then
+        mkdir -p "$PT_output_dir/pe_quick_data"
+        output_dir="$PT_output_dir"
+        output_dir+="/"
+        output_dir+="pe_quick_data"
+    else
+        output_dir="$PT_output_dir"
+        output_dir+="/"
+        output_dir+="pe_quick_data"
+    fi
+
+    count=$(ls -1 "$output_dir"/*.gz 2>/dev/null | wc -l)
+    if [ $count != 0 ]
+    then
+        echo "gz file found for adding node data to"
+    else
+        echo "No gzip files available to use for adding node data"
+    fi
+else
+    echo "No $PT_output_dir directory exists to dump files"
+    exit
+fi
+
+# File variable to use in redirections of command outputs to files
+output_file="$output_dir/pe_server_tuning_status.json"
+
+puppet infrastructure tune --compare --render-as json > $output_file
