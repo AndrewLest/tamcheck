0bc2cf4997ae101487229302c8841bb12b8fc163
Andrew_demo <andrew.lestrange@puppet.com>
2024-07-16 16:21:26 +1000

    Added latest tasks and updated the plan

--- plans/tamcheck_run.pp
+++ plans/tamcheck_run.pp
@@ -11,6 +11,10 @@ plan tamcheck::tamcheck_run (
     run_task('tamcheck::pe_server_name', $targets, output_dir => $output_dir)
     run_task('tamcheck::pe_server_node_count', $targets, output_dir => $output_dir)
     run_task('tamcheck::pe_server_ca_status', $targets, output_dir => $output_dir)
+    run_task('tamcheck::pe_server_details', $targets, output_dir => $output_dir)
+    run_task('tamcheck::pe_server_infrastructure_status', $targets, output_dir => $output_dir)
+    run_task('tamcheck::pe_server_module_list', $targets, output_dir => $output_dir)
+    run_task('tamcheck::pe_server_node_count_not_expired', $targets, output_dir => $output_dir)
     run_task('tamcheck::data_collect', $targets, output_dir => $output_dir)
   }
 }
